6df39f8761d7e631cb52cc9961cf0ddf7d7cdbc6
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-03-06 14:42:36 +0200

    feat: add crt shader

--- roguelike/src/scenes/GameplayUi.ts
+++ roguelike/src/scenes/GameplayUi.ts
@@ -1,6 +1,7 @@
 import { Game, Scene } from 'phaser';
 import { GameManager } from '../game-manager';
 import { GameObjects } from 'phaser';
+import { Shaders } from '../shaders';
 
 /** 
  * Shows adventure log and other UI elements when playing.
@@ -21,12 +22,6 @@ export class GameplayUi extends Scene {
     /** Create UI. */
     create() {
         const { width, height } = this.scale;
-
-        // Empty at first since nothing is selected.
-        this.youSeeText = this.add.text(width * 0.5, height * 0.03, '', {
-            fontSize: 36, color: '#ffffff', align: 'center'
-        })
-            .setOrigin(0.5);
 
         const outlines: GameObjects.Text = this.add.text(0, 0,
             `*---Character---*                  *--Inventory--*
@@ -50,11 +45,22 @@ export class GameplayUi extends Scene {
 *---------------*------------------*-------------*`, {
             color: '#aaaaaa', fontSize: 53
         })
+
+        // Centered circle for placement help
         // this.add.circle(width * 0.5, height * 0.55, 10, 0xffffff, 1)
+
+        const baseShader = new Phaser.Display.BaseShader('Scanlines', Shaders.fragScanlines);
+        this.add.shader(baseShader, 0, 0, width * 1, height * 1).setOrigin(0, 0);
 
         this.logText = this.add.text(width * 0.34, height * 0.64,
             `gets replaced by addLogText(string)`,
-            { fontSize: 42, wordWrap: { useAdvancedWrap: false, width: 600 } });
+            { fontSize: 42, wordWrap: { useAdvancedWrap: false, width: 600 } })
+
+        // Empty at first since nothing is selected.
+        this.youSeeText = this.add.text(width * 0.5, height * 0.03, '', {
+            fontSize: 36, color: '#ffffff', align: 'center'
+        })
+            .setOrigin(0.5);
 
         this.addToLogText("Now adding more stuff.")
         this.addToLogText("Lol1.")
---
+++ roguelike/src/shaders.ts
@@ -0,0 +1,55 @@
+/** Collection of glsl fragment shaders for ease of use. */
+export class Shaders {
+    public static fragScanlines = `#ifdef GL_ES
+    precision mediump float;
+    #endif
+    
+    uniform float time;
+    uniform vec2 resolution;
+    uniform sampler2D iChannel0;
+    uniform sampler2D iChannel1;
+    uniform sampler2D iChannel2;
+    
+    varying vec2 fragCoord;
+    
+    // Fork of "Fast CRT" by kbjwes77. https://shadertoy.com/view/WsVSzV
+    // 2025-03-06 11:43:11
+    // Original shader by default Shadertoy licence CC BY NC SA 3.0 Unported
+    // Additional modifications by madmarcel (https://phaser.io/sandbox/ShtRd6RU)
+    // Refactored and tweaked by r10z
+    
+    float curvature = 0.40;
+    float scanlineDarkness = 0.35;
+    
+    void mainImage(out vec4 fragColor,in vec2 fragCoord)
+    {
+        // squared distance from center
+        vec2 uv = fragCoord/resolution.xy;
+        // distance (from) center
+        vec2 dc = abs(0.5 - uv);
+        dc *= dc;
+        
+        uv.x -= 0.5;
+        uv.x *= 1.0 + (dc.y * (0.3 * curvature));
+        uv.x += 0.5;
+        uv.y -= 0.5;
+        uv.y *= 1.0 + (dc.x * (0.4 * curvature));
+        uv.y *= -1.0; // otherwise upside down
+        uv.y += 0.5;
+    
+        if (uv.y > 1.0 || uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0)
+        {
+            fragColor = vec4(0.00, 0.000, 0.000, 1.0);
+            return;
+        }
+            
+        float apply = abs(sin(fragCoord.y) * scanlineDarkness);
+        fragColor = vec4(mix(texture2D(iChannel0,uv).rgb, vec3(0.0) , apply), 1.0);
+    }   
+    
+    void main(void)
+    {
+        mainImage(gl_FragColor, fragCoord.xy);
+    }
+    `
+}
